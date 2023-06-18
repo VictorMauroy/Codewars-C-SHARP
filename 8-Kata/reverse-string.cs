9a2382f01b30ade252c9b09c22f94d1da7fc9c4d
VictorMauroy <mauroy.victor@gmail.com>
2023-06-18 16:39:47 +0200

    Feat: :sparkles: add a reverse string function

---
+++ 8-Kata/reverse-string.cs
@@ -0,0 +1,18 @@
+/* Description :
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+using System;
+
+public static class Kata
+{
+    public static string Solution(string str) 
+    {
+    char[] charArray = str.ToCharArray();
+    Array.Reverse(charArray);
+    return string.Join("", charArray);
+    }
+}
