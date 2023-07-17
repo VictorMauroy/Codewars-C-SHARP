e864daaddc26dbf2235f686fc688afb96d8a69a7
VictorMauroy <mauroy.victor@gmail.com>
2023-07-17 18:48:12 +0200

    docs: :memo: add comments to describe snail sort method and add some good ideas from other devs

--- 4-Kata/SnailSort.cs
+++ 4-Kata/SnailSort.cs
@@ -1,3 +1,23 @@
+/* Description:
+Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+For better understanding, please follow the numbers of the next array consecutively:
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9]
+
+NOTE: The idea is not sort the elements from the lowest value to the highest; 
+the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+
+NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+*/
+
 public static class SnailSort
 {
     public static int[] Snail(int[][] array)
@@ -58,15 +78,51 @@ public static class SnailSort
             if (selector > 3) selector = 0;
         }
 
-
-/*            string result = "List with { ";
-        for (int i = 0; i < snailList.Count; i++)
-        {
-            result += snailList[i].ToString() + ", ";
-        }
-        result += " }";
-        Console.WriteLine(result); //DEBUG*/
-
         return snailList.ToArray();
     }
 }
+
+
+/* Good ideas:
+*
+public static IEnumerable<int> Snail(int[][] a, int r = 0)
+{
+    int n = a[0].Length - 1 - 2 * r;
+    if (n < 0) return new int[0];
+    if (n == 0) return new []{a[r][r]};
+    
+    var sides = new []{
+    a[r],                             // Top
+    a.Select(x => x[r+n]),            // Right
+    a[r+n].Reverse(),                 // Bottom
+    a.Select(x => x[r]).Reverse()     // Left
+    }
+    .SelectMany(x => x.Skip(r).Take(n));
+
+    return (n == 1) ? sides : sides.Concat(Snail(a, r+1));
+}
+
+
+
+
+public static int[] Snail(int[][] array)
+{
+    int l = array[0].Length;
+    int[] sorted = new int[l * l];
+    Snail(array, -1, 0, 1, 0, l, 0, sorted);
+    return sorted;
+}
+public static void Snail(int[][] array, int x, int y, int dx, int dy, int l, int i, int[] sorted)
+{
+    if (l == 0)
+        return;
+    for (int j = 0; j < l; j++)
+    {
+        x += dx;
+        y += dy;
+        sorted[i++] = array[y][x];
+    }
+    Snail(array, x, y, -dy, dx, dy == 0 ? l - 1 : l, i, sorted);
+}
+
+*/
