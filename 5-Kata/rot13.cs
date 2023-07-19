513999980de394fb447d93bcbc9cd63a224ebad4
VictorMauroy <mauroy.victor@gmail.com>
2023-07-19 18:31:33 +0200

    docs: :memo: update comments to add a description and a clever idea

--- 5-Kata/rot13.cs
+++ 5-Kata/rot13.cs
@@ -1,3 +1,12 @@
+/* Description :
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. 
+ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. 
+If there are numbers or special characters included in the string, they should be returned as they are. 
+Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+*/
+
 using System;
 using System.Linq;
 using System.Collections.Generic;
@@ -5,5 +14,22 @@ using System.Collections.Generic;
 public class Kata
 {
   public static string Rot13(string message)
-  =>  String.Join("", message.Select(c => Char.IsLetter(c) ? ((char)(((int)Char.ToUpper(c) - 65 + 13) < 26 ? (int)c + 13 : (int)c + 13 - 26)) : c ));
+  => String.Join("", message.Select(c => Char.IsLetter(c) ? ((char)(((int)Char.ToUpper(c) - 65 + 13) < 26 ? (int)c + 13 : (int)c + 13 - 26)) : c ));
 }
+
+/* Best:
+
+public static string Rot13(string message)
+{
+    string result = "";
+    foreach (var s in message)
+    {
+        if ((s >= 'a' && s <= 'm') || (s >= 'A' && s <= 'M'))
+            result += Convert.ToChar((s + 13)).ToString();
+        else if ((s >= 'n' && s <= 'z') || (s >= 'N' && s <= 'Z'))
+            result += Convert.ToChar((s - 13)).ToString();
+        else result += s;
+    }
+    return result;
+}
+*/
