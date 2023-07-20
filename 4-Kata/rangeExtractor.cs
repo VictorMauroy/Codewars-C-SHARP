0b57c8428f73e78ebe59b5507fecedad7091fa72
VictorMauroy <mauroy.victor@gmail.com>
2023-07-20 16:41:01 +0200

    feat: :sparkles: add complete version of range extraction, but ugly

--- 4-Kata/rangeExtractor.cs
+++ 4-Kata/rangeExtractor.cs
@@ -1,35 +1,48 @@
-// Ongoing
+/* Description:
+
+*/
 
 public class RangeExtraction
 {
     public static string Extract(int[] numArray)
     {
-      string result = numArray[0].ToString();
-      int range = 0; 
+      string result = "";
+      int range = 0;
+      int rangeStart = 0;
       
-      for(int i = 1; i < numArray.Length; i++)
+      for(int i = 0; i < numArray.Length; i++)
       {
-        if(numArray[i-1]+1 == numArray[i]){
-          range++;
-          if(range < 2 && i == numArray.Length-1){
-            result += "," + numArray[i];
-          } else if(i == numArray.Length-1){
-           result += "-" + numArray[i];
-          }
-        } 
-        else {
+        //When it reach the end of the array.
+        if(i+1 == numArray.Length) {
           if(range > 1){
-            range = 0;
-            result += "-" + numArray[i-1] + "," + numArray[i];
-          } else if(range == 1) 
-          {
-            result += "," + numArray[i-1] + "," + numArray[i];
-          } 
-          else 
-          {
-            result += "," + numArray[i];
+            result += rangeStart + "-" + numArray[i];
+          } else if(range > 0) {
+            result += numArray[i-1] + "," + numArray[i];
+          } else {
+            result += numArray[i];
           }
+          break;
         }
+        
+        if(numArray[i]+1 == numArray[i+1])
+        {
+          if(range == 0){
+            rangeStart = numArray[i];
+          }
+          range++;
+        } 
+        else 
+        {
+          if(range == 0){
+            result += numArray[i] + ",";
+          } else if(range == 1){
+            result += numArray[i-1] + "," + numArray[i] + ",";
+          } else {
+            result += rangeStart + "-" + numArray[i]  + ",";
+          }
+          range = 0;
+        }
+        
       }
       
       return result;
