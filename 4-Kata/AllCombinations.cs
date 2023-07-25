f9c0e0ac65438f855586dac2aacdd11e02c8d9d8
VictorMauroy <mauroy.victor@gmail.com>
2023-07-25 19:42:34 +0200

    docs: :memo: add clever or best practice of others users

--- 4-Kata/AllCombinations.cs
+++ 4-Kata/AllCombinations.cs
@@ -22,3 +22,27 @@ class Permutations
     return branchs;
   }
 }
+
+
+/* BEST OR CLEVER */
+public static List<string> SinglePermutations(string s) => $"{s}".Length < 2 ?
+  	new List<string> { s } :
+  	SinglePermutations(s.Substring(1))
+  		.SelectMany(x => Enumerable.Range(0, x.Length + 1)
+  			.Select((_, i) => x.Substring(0, i) + s[0] + x.Substring(i)))
+  		.Distinct()
+  		.ToList();
+    
+
+public static List<string> SinglePermutations( string s ) {
+        if ( s.Length < 2 ) {
+            return new List<string> {s};
+        }
+        var result = new HashSet<string>( );
+        foreach ( var sub in SinglePermutations( s.Substring( 1 ) ) ) {
+            for ( int i = 0; i <= sub.Length; i++ ) {
+                result.Add( sub.Substring( 0, i ) + s [ 0 ] + sub.Substring( i ) );
+            }
+        }
+        return result.ToList( );
+    }
