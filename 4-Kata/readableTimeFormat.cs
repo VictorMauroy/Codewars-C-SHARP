b37eb416afb26471c3a3080bcf1495bad28d76ff
VictorMauroy <mauroy.victor@gmail.com>
2023-08-23 14:55:58 +0200

    docs: :memo: add a description for the last kata and an interesting other answer

--- 4-Kata/readableTimeFormat.cs
+++ 4-Kata/readableTimeFormat.cs
@@ -1,3 +1,35 @@
+/*
+Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+The function must accept a non-negative integer. If it is zero, it just returns "now". 
+Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+It is much easier to understand with an example:
+
+* For seconds = 62, your function should return 
+    "1 minute and 2 seconds"
+* For seconds = 3662, your function should return
+    "1 hour, 1 minute and 2 seconds"
+For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+Note that spaces are important.
+
+Detailed rules
+The resulting expression is made of components like 4 seconds, 1 year, etc. 
+In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
+
+The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
+
+A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
+
+Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
+
+A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
+
+A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. 
+Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
+*/
+
 using System;
 using System.Collections.Generic;
 
@@ -43,4 +75,26 @@ public class HumanTimeFormat{
     return sentence;
   }
   
+}
+
+
+/* Other ideas  (not by me) */
+public static string formatDuration(int sec)
+{
+    if (sec == 0) return "now";
+    var L = new List<string>();
+    var t = TimeSpan.FromSeconds(sec);
+    int y = t.Days / 365;
+    int d = t.Days % 365;
+    int h = t.Hours;
+    int m = t.Minutes;
+    int s = t.Seconds;
+    if (y > 0) L.Add(y+" year"+(y>1?"s":""));
+    if (d > 0) L.Add(d+" day"+(d>1?"s":""));
+    if (h > 0) L.Add(h+" hour"+(h>1?"s":""));
+    if (m > 0) L.Add(m+" minute"+(m>1?"s":""));
+    if (s > 0) L.Add(s+" second"+(s>1?"s":""));
+    
+    return string.Join(", ", L.Take(L.Count() - 1)) + (L.Count()>1?" and ":"") + L.Last(); ;
+
 }
