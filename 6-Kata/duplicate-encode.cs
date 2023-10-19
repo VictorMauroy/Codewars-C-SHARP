14a1150d1e186fc56ab63ddcce6539ab737dcca5
VictorMauroy <mauroy.victor@gmail.com>
2023-10-19 20:50:31 +0200

    docs: :memo: add description and one line code for duplicate encode function

--- 6-Kata/duplicate-encode.cs
+++ 6-Kata/duplicate-encode.cs
@@ -1,3 +1,15 @@
+/* Description :
+The goal of this exercise is to convert a string to a new string where each character 
+in the new string is "(" if that character appears only once in the original string, or ")" 
+if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+*/
+
 using System.Collections.Generic;
 using System.Linq;
 
@@ -23,3 +35,10 @@ public class Kata
                .Select(c => characterOccurence[c] > 1 ? ")" : "(").ToList());
   }
 }
+
+
+/* Interesting ideas */
+public static string DuplicateEncode(string word)
+{  
+    return new string(word.ToLower().Select(ch => word.ToLower().Count(innerCh => ch == innerCh) == 1 ? '(' : ')').ToArray());
+} //One line but uh... that seems pretty heavy
