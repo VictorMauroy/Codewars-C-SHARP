6e5e63c89631eab148c304cba62c577159eea8aa
VictorMauroy <mauroy.victor@gmail.com>
2023-11-27 17:58:58 +0100

    feat: :sparkles: add second attempt for last kata, shortened answer

--- 4-Kata/stripComments.cs
+++ 4-Kata/stripComments.cs
@@ -19,3 +19,14 @@ public class StripCommentsSolution
       return string.Join("\n", editedLines);
     }
 }
+
+
+/* Second attempt => shortened */
+using System.Linq;
+using System.Text.RegularExpressions;
+public static string StripComments(string text, string[] commentSymbols)
+    => string.Join("\n", Regex.Split(text, @"\n")
+          .Select(line => Regex.Replace(
+            line, 
+            @"\s*([" + string.Concat(commentSymbols) + @"].*)|(\s*$)",
+            "")));
