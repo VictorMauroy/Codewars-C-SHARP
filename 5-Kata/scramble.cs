a3e64da0e3dc3f782f790284c802b5c3823f2cca
VictorMauroy <mauroy.victor@gmail.com>
2023-12-01 17:04:19 +0100

    feat: :sparkles: add solution for kata scramble

--- 5-Kata/scramble.cs
+++ 5-Kata/scramble.cs
@@ -11,3 +11,27 @@ public static bool Scramble(string str1, string str2)
     
     return orderedStr.Count() == intersection.Count();
 }
+
+// Final answer :
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class Scramblies 
+{
+    
+    public static bool Scramble(string str1, string str2) 
+    {
+      int matchCount = 0;
+      List<char> letters = str1.Select(c => c).ToList();
+      
+      foreach(char c in str2){
+        if(letters.Contains(c)){
+          matchCount++;
+          letters.Remove(c);
+        }
+      }
+      
+      return matchCount == str2.Length;
+    }
+}
