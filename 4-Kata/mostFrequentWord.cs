3e34a6e32cde78488d001790a1113ce8749a6ed9
VictorMauroy <mauroy.victor@gmail.com>
2023-12-06 17:43:11 +0100

    fix: :bug: resolve error about multiple quote being validated as a word

--- 4-Kata/mostFrequentWord.cs
+++ 4-Kata/mostFrequentWord.cs
@@ -7,12 +7,12 @@ using System.Text.RegularExpressions;
 public class TopWords
 {
     public static List<string> Top3(string text)
-      {
-          List<string> words = Regex.Split(text, @"[^\w']+").ToList();
-          return (from word in words
-                  where !string.IsNullOrWhiteSpace(word)
-                  group word.ToLower() by word into newGroup
-                  orderby newGroup.Count() descending
-                  select newGroup.Key).Take(3).ToList();
-      }
+    {
+        List<string> words = Regex.Matches(text, @"(\w+'{0,1}\w*)").Select(m => m.Value.ToLower()).ToList();
+        return (from word in words
+              where !string.IsNullOrWhiteSpace(word)
+              group word by word into newGroup
+              orderby newGroup.Count() descending
+              select newGroup.Key).Take(3).ToList();
+    }
 }
