48164e3fe05234f39240e1d7a6745521b8ea3fa8
VictorMauroy <mauroy.victor@gmail.com>
2023-12-06 14:19:47 +0100

    feat: :construction: resolve not Icomparable error

--- 4-Kata/mostFrequentWord.cs
+++ 4-Kata/mostFrequentWord.cs
@@ -3,14 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+
 public class TopWords
 {
     public static List<string> Top3(string text)
-    {
-      List<string> words = Regex.Split(text, @"[^\w']").ToList();
-      return (from word in words
-        group word by word into newGroup
-        orderby newGroup descending
-        select newGroup.Key).Take(3).ToList();
-    }
+      {
+          List<string> words = Regex.Split(text, @"[^\w']+").ToList();
+          return (from word in words
+                  group word.ToLower() by word into newGroup
+                  orderby newGroup.Count() descending
+                  select newGroup.Key).Take(3).ToList();
+      }
 }
