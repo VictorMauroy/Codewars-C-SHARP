1e547379e8eade780d42117f5a2a32b0c16a8932
VictorMauroy <mauroy.victor@gmail.com>
2023-12-12 16:35:18 +0100

    docs: :memo: add other solutions for last kata

--- 5-Kata/isAlphanumeric.cs
+++ 5-Kata/isAlphanumeric.cs
@@ -1,5 +1,6 @@
 /* Description:
-In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+In this example you have to validate if a user input string is alphanumeric. 
+The given string is not nil/null/NULL/None, so you don't have to check that.
 
 The string has the following conditions to be alphanumeric:
 
@@ -16,3 +17,19 @@ public class Kata
   public static bool Alphanumeric(string str)
   => (str != "") ? !Regex.IsMatch(str, @"(\W|_)") : false;
 }
+
+
+
+// Other solutions (not by me)
+
+public static bool Alphanumeric(string str)
+{
+    return str.All(c => Char.IsLetterOrDigit(c)) && !string.IsNullOrEmpty(str);
+}
+
+public class Kata
+{
+  public static bool Alphanumeric(string str) =>
+    new Regex("^[a-zA-Z0-9]+$").Match(str).Success;
+}
+// I knew I could have done it without the str != "" condition
